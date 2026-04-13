c3f0ac96b647bf6bf060f478e8742e6a9fcdd685
Matey Karassimeonov <182628672+virtode@users.noreply.github.com>
2026-04-13 15:16:32 +0200

    fix mobile scroll

--- app/(app)/contacts/page.tsx
+++ app/(app)/contacts/page.tsx
@@ -184,7 +184,7 @@ function ContactsPageContent() {
       </div>
 
       {/* Table */}
-      <div className="flex-1 min-h-0 overflow-y-auto">
+      <div className="flex-1 min-h-0 overflow-hidden">
         <ContactsTable
         contacts={data?.contacts ?? []}
         totalCount={data?.total ?? 0}
